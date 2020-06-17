1956661fc34a061ff33478fe9074de2dd207e2f6
Evant.J <EJ80054@student.uph.edu>
2020-06-17 14:33:58 +0700

    index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "AAAAAAAAAAAAAAAAA";
-?>
+<!DOCTYPE html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="CSS/main.css">
+    </head>
+    <body>
+        <?php
+            require("header.php");
+        ?> 
+        <?php
+            require("footer.php");
+        ?> 
+    </body>
+</html>
