ee38f57ef02f7164f3bcb48e61e5eae8f606a174
Kevin <kevin_huang31@yahoo.co.id>
2020-06-17 17:13:12 +0700

    added bootstrap file edited contact edited registration

--- ctc.php
+++ ctc.php
@@ -1,7 +1,25 @@
 <!DOCTYPE html>
     <head>
-        <title></title>
+        <title>Contact</title>
+        <link rel="stylesheet" href="CSS/bootstrap.css">
     </head>
     <body>
+        <?php require("Layout\\header.php"); ?> 
+        <div class="container">
+            <div class="display-4" style="margin: 1rem; text-align: center">Hubungi Kami</div>
+            <ul class="list-group">
+                <li class="list-group-item"><div>Email: </div> hangkesturi69@admin.hk.edu </li>
+                <li class="list-group-item">
+                    <div>Telepon: </div>
+                    <div>(+62)61 4538132 (Kantor)<div>
+                    <div>(+62)853 523 5421 (Abdul)<div>
+                    <div>(+62)823 531 5164 (Joko)<div>
+                </li>
+                <li class="list-group-item"><div>Facebook: </div>hangkesturi69 </li>
+                <li class="list-group-item"><div>Twitter: </div>@hkesturi69 </li>
+                <li class="list-group-item"><div>Lokasi: </div>Jl. Sutomo No. 169, Medan, Sumatera Utara </li>
+            </ul>
+        </div>
+        <?php require("Layout\\footer.php"); ?> 
     </body>
 </html>
--- regis.php
+++ regis.php
@@ -1,7 +1,63 @@
 <!DOCTYPE html>
     <head>
-        <title></title>
+        <title>Contact</title>
+        <link rel="stylesheet" href="CSS/bootstrap.css">
     </head>
     <body>
+        <?php require("Layout\\header.php"); ?> 
+        <div class="container">
+            <div class="display-4" style="margin: 1rem; text-align: center">Daftar Sekarang</div>
+            <form>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nama Depan &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nama Belakang</span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div>  
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">No. Telepon Rumah  (Dengan Kode Area) &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div>  
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">No. Handphone &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div> 
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Alamat E-mail &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div>                                                                  
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Konfirmasi Alamat E-mail &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                </div>                 
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Kelas</label>
+                     </div>
+                    <select class="custom-select" id="inputGroupSelect01">
+                        <option selected>Pilih...</option>
+                        <option value="1">I - SD</option>
+                        <option value="2">II - SD</option>
+                        <option value="3">III - SD</option>
+                    </select>
+                    </div>                         
+                <button class="btn btn-primary">Submit</button>                           
+            </form>
+        </div>
+        <?php require("Layout\\footer.php"); ?> 
     </body>
 </html>
