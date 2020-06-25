69f3e5db62300ef27f3483c9f5faf7684a586125
Evant.J <EJ80054@student.uph.edu>
2020-06-25 19:54:45 +0700

    edit

--- db.admin.php
+++ db.admin.php
@@ -25,112 +25,122 @@ if (isset($_POST['regis'])) {
 }
 */
 
-if (isset($_POST['approve'])) {
-	approve();
+// if (isset($_POST['approve'])) {
+// 	approve();
+// }
+// if (isset($_POST['hapus'])) {
+// 	hapus();
+// }
+if (isset($_POST['update'])) {
+	// update();
+	$id = $_POST['id'];
+	$name = $_POST['name'];
+	$phone_number = $_POST['phone_number'];
+	$email = $_POST['email'];
+	$status = $_POST['status'];
+
+	mysqli_query($conn, "UPDATE siswa SET name='$name', phone_number='$phone_number', email='$email', status='$status' WHERE id=$id");
+	$_SESSION['message'] = "Data updated!"; 
+	header('location: admin.php');
 }
-if (isset($_POST['hapus'])) {
-	hapus();
+
+if (isset($_POST['del'])) {
+	$id = $_POST['del'];
+	mysqli_query($conn, "DELETE FROM siswa WHERE id=$id");
+	$_SESSION['message'] = "Data deleted!"; 
+	header('location: admin.php');
 }
 
-function approve()
-{
-	global $conn, $errors, $username, $name_error;
+// function approve()
+// {
+// 	global $conn, $errors, $username, $name_error;
 
-	$query = "UPDATE siswa SET status = true
-                    WHERE id = '$id'";
-	$results = mysqli_query($conn, $query);
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES['kartu_keluarga']["name"]);
+// 	$query = "UPDATE siswa SET status = true
+//                     WHERE id = '$id'";
+// 	$results = mysqli_query($conn, $query);
+// 	$target_dir = "uploads/";
+// 	$target_file = $target_dir . basename($_FILES['kartu_keluarga']["name"]);
 
-	//$uploadOK = 1;
-	move_uploaded_file($_FILES["kartu_keluarga"]["tmp_name"], $target_file);
+// 	//$uploadOK = 1;
+// 	move_uploaded_file($_FILES["kartu_keluarga"]["tmp_name"], $target_file);
 			
-	echo "<pre>"; 
-	print_r($_FILES); 
-	echo "</pre>"; 
-    header('location: admin.php');
-}
+// 	echo "<pre>"; 
+// 	print_r($_FILES); 
+// 	echo "</pre>"; 
+//     header('location: admin.php');
+// }
 
-// REGISTER USER
-function register(){
-	// call these variables with the global keyword to make them available in function
-    global $conn, $errors, $username, $name_error;
+// function update(){
+// 	// call these variables with the global keyword to make them available in function
+// 	global $conn, $errors, $username, $name_error;
 
-    //angkasa
+// 	// receive all input values from the form. Call the e() function
+//     // defined below to escape form values
+// 	$name    =  e($_POST['name']);
+//     $phone_number    =  e($_POST['phone_number']);
+//     $email    =  e($_POST['email']);
+// 	// $category    =  e($_POST['category']);
+// 	$status    =  e($_POST['status']);
+// 	// form validation: ensure that the form is correctly filled
+// 	if (empty($name)) { 
+// 		array_push($errors, "Belum Lengkap"); 
+// 	}
+// 	if (empty($phone_number)) { 
+// 		array_push($errors, "Belum Lengkap"); 
+//     }
+//     if (empty($email)) { 
+// 		array_push($errors, "Belum Lengkap"); 
+//     }
+//     // if (empty($category)) { 
+// 	// 	array_push($errors, "Belum Lengkap"); 
+// 	// }
+//     if (empty($status)) { 
+// 		array_push($errors, "Belum Lengkap"); 
+// 	}
+// 	// register user if there are no errors in the form
+// 	if (count($errors) == 0) {
+// 		$uname = $_SESSION['username'];
+//         $sql_u = "SELECT * FROM siswa WHERE username='$uname'";
+//         $res_u = mysqli_query($conn, $sql_u);
+//         if (mysqli_num_rows($res_u) == 0) {
+//         	$name_error = "Belum Lengkap"; 		
+//         } else{
+// 			// $target_dir = "uploads/";
+// 			// $kk_file = $target_dir . $uname . '_kartu_keluarga';
+// 			// $ij_file = $target_dir . $uname . '_ijazah';
+// 			// $kk_type = explode("/", $_FILES["kartu_keluarga"]["type"], 2);
+// 			// $ij_type = explode("/", $_FILES["ijazah"]["type"], 2);
+// 			// $kk = $kk_file . "." . $kk_type[1];
+// 			// $ij = $ij_file . "." . $ij_type[1];
 
-   
-    //angkasa source : https://www.w3schools.com/php/func_mysqli_fetch_array.asp
+// 			// print_r($_FILES["ijazah"]["type"]);
+// 			// print_r($_FILES["kartu_keluarga"]["type"]);
 
-
-	// receive all input values from the form. Call the e() function
-    // defined below to escape form values
-	$name    =  e($_POST['name']);
-    $phone_number    =  e($_POST['phone_number']);
-    $email    =  e($_POST['email']);
-	$category    =  e($_POST['category']);
-	$kartu_keluarga    =  e($_POST['kartu_keluarga']);
-	$ijazah = e($_POST['ijazah']);
-	// form validation: ensure that the form is correctly filled
-	if (empty($name)) { 
-		array_push($errors, "Belum Lengkap"); 
-	}
-	if (empty($phone_number)) { 
-		array_push($errors, "Belum Lengkap"); 
-    }
-    if (empty($email)) { 
-		array_push($errors, "Belum Lengkap"); 
-    }
-    if (empty($category)) { 
-		array_push($errors, "Belum Lengkap"); 
-	}
-	if (empty($kartu_keluarga)) { 
-		array_push($errors, "Belum Lengkap"); 
-    }
-    if (empty($ijazah)) { 
-		array_push($errors, "Belum Lengkap"); 
-    }
-    $sql_u = "SELECT * FROM siswa;"; //WHERE username='$uname'";
-        $result = mysqli_query($conn, $sql_u);
-        $resultCheck = mysqli_num_rows($result);
-        if($resultCheck > 0)
-        {
-            while ($row = mysqli_fetch_assoc($result))
-            {
-                echo $row['user_uid'] . "<br>";
-            }
-        }
-	// register user if there are no errors in the form
-	if (count($errors) == 0) {
-		//$uname = $_SESSION['username'];
-        
-        
-        
-    
-        
-
-        
-        if (mysqli_num_rows($res_u) == 0) {
-        	$name_error = "Belum Lengkap"; 		
-        } else{
-            /*
-            $query = "UPDATE siswa SET name = '$name', phone_number = '$phone_number', email = '$email',
-					kartu_keluarga = '$kartu_keluarga', ijazah = '$ijazah'
-                    WHERE username = '$uname'";
-			$results = mysqli_query($conn, $query);
-			$target_dir = "uploads/";
-			$target_file = $target_dir . basename($_FILES['kartu_keluarga']["name"]);
-
-			//$uploadOK = 1;
-			move_uploaded_file($_FILES["kartu_keluarga"]["tmp_name"], $target_file);
+// 			// $uploadOK = 1;
 			
-			echo "<pre>"; 
-			print_r($_FILES); 
-			echo "</pre>"; 
-            //header('location: bayar.php');
-            */
-        }			
-	}
-}
+// 			// if(getimagesize($_FILES["kartu_keluarga"]["tmp_name"]) == false || getimagesize($_FILES["ijazah"]["tmp_name"]) == false){
+// 			// 	$uploadOK = 0;
+// 			// }
+
+// 			// if($uploadOK == 0){
+// 			// 	echo('<script>alert("Upload Failed");</script>');
+// 			// 	echo('<script>location="regis.php";</script>');
+// 			// } else {
+// 			// 	if(move_uploaded_file($_FILES["kartu_keluarga"]["tmp_name"], $kk) && move_uploaded_file($_FILES["ijazah"]["tmp_name"], $ij)){
+// 			// 		$uploadOK = 1;
+// 			// 	} else {
+// 			// 		echo('<script>alert("Upload Failed");</script>');
+// 			// 		echo('<script>location="regis.php";</script>');
+// 			// 		$uploadOK = 0;
+// 			// 	}
+// 			// }
+// 			$query = "UPDATE siswa SET name = '$name', phone_number = '$phone_number',  email = '$email', status = '$status'
+// 			WHERE username = '$uname'";
+// 			$results = mysqli_query($conn, $query);			
+// 			// header('location: bayar.php');
+//         }			
+// 	}
+// }
 
 // escape string
 function e($val){
@@ -148,4 +158,5 @@ function display_error() {
 			}
 		echo '</div>';
 	}
-}	
+}
+
---
+++ edit.php
@@ -0,0 +1,130 @@
+<?php require "db.admin.php";
+    // if (isset($_REQUEST['id'])) {
+    //     $id = $_REQUEST['id'];
+    //     $update = true;
+    //     $record = mysqli_query($conn, "SELECT * FROM siswa WHERE id=$id");
+        
+    //     if (count($record) == 1 ) {
+    //         $n = mysqli_fetch_array($record);
+    //         $name = $n['name'];
+    //         $birthdate = $n['birthdate'];
+    //         $email = $n['email'];
+    //         $phone_number = $n['phone_number'];
+    //         $address = $n['address'];
+    //         $category = $n['category'];
+    //     }
+    // }
+    $id=$_REQUEST['edit'];
+    $query = "SELECT * from siswa where id='".$id."'"; 
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+?>
+<!DOCTYPE html>
+<style>
+.form_error span {
+        width: 80%;
+        height: 35px;
+        margin: 3px 10%;
+        font-size: 1.1em;
+        color: #D83D5A;
+    }
+    .form_error input {
+        border: 1px solid #D83D5A;
+    }
+</style>
+    <head>
+        <title>Contact</title>
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/main.css">
+    </head>
+    <body>
+        <?php require("Layout\\header.php"); ?> 
+
+<div class="container" style="margin-top: 70px;">
+    <div class="display-4" style="margin: 1rem; text-align: center">Daftar Sekarang</div>
+    
+    <form method="post" action="db.admin.php" id="regis_form" enctype="multipart/form-data">
+    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+
+            <?php /* echo display_error(); */ ?>
+
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend" <?php if (isset($name_error)): ?> class="form_error" <?php endif ?>>
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nama Lengkap &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name = "name" value="<?php echo  $row['name']; ?>">
+                </div>
+                
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend"  <?php if (isset($name_error)): ?> class="form_error" <?php endif ?>>
+                        <span class="input-group-text" id="inputGroup-sizing-default">No. Telepon Rumah  (Dengan Kode Area) &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name = "phone_number" value="<?php echo $row['phone_number']; ?>">
+                </div>  
+
+                
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend" <?php if (isset($name_error)): ?> class="form_error" <?php endif ?>>
+                        <span class="input-group-text" id="inputGroup-sizing-default">Alamat E-mail &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" name = "email" value="<?php echo $row['email']; ?>">
+                </div>             
+
+                        
+
+                <!-- <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Kelas</label>
+                     </div>
+                    <select class="custom-select" id="inputGroupSelect01" name = "category" value="<?php echo $row['category']; ?>">
+                        <option selected>Pilih...</option>
+                        <option value="1">I - SD</option>
+                        <option value="2">II - SD</option>
+                        <option value="3">III - SD</option>
+                        <option value="4">IV - SD</option>
+                        <option value="5">V - SD</option>
+                        <option value="6">VI - SD</option>
+                    </select>
+                </div> -->
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <label class="input-group-text" for="inputGroupSelect01">Status</label>
+                     </div>
+                    <select class="custom-select" id="inputGroupSelect01" name = "status">
+                        <option selected>Pilih...</option>
+                        <option value="1">Terima</option>
+                        <option value="2">Tolak</option>
+                    </select>
+                </div>
+
+                <!-- <div class="input-group mb-3">
+                    <div class="input-group-prepend" <?php if (isset($name_error)): ?> class="form_error" <?php endif ?>>
+                        <span class="input-group-text" id="inputGroup-sizing-default">Kartu Keluarga &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    Select file to upload:
+                    <input type="file" name="kartu_keluarga" id="fileToUpload1">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend" <?php if (isset($name_error)): ?> class="form_error" <?php endif ?>>
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nilai Rapor &nbsp; <div style="color:red">*</div></span>
+                    </div>
+                    Select file to upload:
+                    <input type="file" name="ijazah" id="fileToUpload2">
+                </div>   -->
+ 
+                    
+                <button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
+                <!-- <?php if ($update == true): ?> -->
+<!-- <?php else: ?> -->
+	<!-- <button class="btn" type="submit" name="save" >Save</button> -->
+<!-- <?php endif ?>                          -->
+            </form>
+        </div>
+
+        <?php require("Layout\\footer.php"); ?> 
+    </body>
+</html>
